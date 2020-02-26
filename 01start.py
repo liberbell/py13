2433a41dcc6256ea7267e7050d1173ac629913c4
Liberbell <liberbell@gmail.com>
2020-02-27 07:38:40 +0900

    start7

--- 01start.py
+++ 01start.py
@@ -1,8 +1,11 @@
-isRaining = True
+isRaining = False
 isSunny = False
 
 if isRaining and isSunny:
     print("We might see a Rainbow. ")
 
-if isRaining or isRaining:
+if isRaining or isSunny:
     print("It might be rainy and it might be sunny.")
+
+if not isRaining:
+    print("It must be raining.")

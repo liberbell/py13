908188cdeef01bb88b09451bc2a870a86a37f0b6
Liberbell <liberbell@gmail.com>
2020-02-27 07:41:30 +0900

    startr8

--- 01start.py
+++ 01start.py
@@ -9,3 +9,9 @@ if isRaining or isSunny:
 
 if not isRaining:
     print("It must be raining.")
+
+ages = [12, 18, 28, 39, 87, 7, 2]
+for age in ages:
+    isAdult = age > 17;
+    if not isAdult:
+        print("Being " + str(age) + " does not make you an adult.")

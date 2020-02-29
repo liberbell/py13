23a865c4460ed50f8793ebb3e89a4eb202d13d05
Liberbell <liberbell@gmail.com>
2020-02-29 16:13:58 +0900

    compare1

--- compare.py
+++ compare.py
@@ -4,3 +4,11 @@ print(len(firstname))
 lastname = "Swift"
 print(len(lastname))
 firstname.__len__()
+
+
+ages = [0, 11, 22, 45, 87, 4]
+print(len(ages))
+
+i = 0
+for i in range(0, ages):
+    print(i)

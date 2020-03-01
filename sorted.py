bdb353dccae19653745ea373a499c59719dd496b
Liberbell <liberbell@gmail.com>
2020-03-02 06:45:52 +0900

    sorted7

--- sorted.py
+++ sorted.py
@@ -8,3 +8,6 @@ print(sorted(["Sue", "jerry", "linda"]))
 
 print(sorted("My favorite friend is Linda".split(), key=str.upper))
 print(sorted(pointInGame, reverse=True))
+
+leaderBoard = {211: "CKL", 134: "DIE", 331: "JFK"}
+print(sorted(leaderBoard))

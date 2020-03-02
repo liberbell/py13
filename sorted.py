77a42ce919e3d1909ac974fa35c114e68ff2f7b7
Liberbell <liberbell@gmail.com>
2020-03-03 07:00:38 +0900

    sorted2

--- sorted.py
+++ sorted.py
@@ -12,3 +12,6 @@ print(sorted(pointInGame, reverse=True))
 leaderBoard = {211: "CKL", 134: "DIE", 331: "JFK"}
 print(sorted(leaderBoard, reverse=True))
 print(leaderBoard.get(134))
+
+students = [('alice', 'B', 12), ('eliza', 'A', 16), ('tae', 'c', 15)]
+print(sorted(students, key=lambda, student:student[0]))

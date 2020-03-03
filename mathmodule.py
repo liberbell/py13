932dc5f82864c6f5af1d7b4ddcce9f28dad02bec
Liberbell <liberbell@gmail.com>
2020-03-04 07:27:44 +0900

    mathmodule6

--- mathmodule.py
+++ mathmodule.py
@@ -10,3 +10,9 @@ print(-math.inf)
 obst_direction = math.cos(math.pi / 4)
 print(obst_direction)
 print(math.sin(math.pi / 4))
+
+cookies = 10.3
+candy = 7
+
+print(math.ceil(cookies))
+print(math.ceil(candy))

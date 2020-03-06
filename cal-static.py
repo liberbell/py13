233cfe08c4f2c7be50887c4d23e1868f79e5e968
Liberbell <liberbell@gmail.com>
2020-03-06 10:48:12 +0900

    cal-static2

--- cal-static.py
+++ cal-static.py
@@ -1,5 +1,5 @@
 import statistics
 agesData = [10, 13, 14, 24, 11, 7, 5, 8]
-# print(statistics.mean(agesData))
-# print(statistics.mode(agesData))
-# print(statistics.median(agesData))
+print(statistics.mean(agesData))
+print(statistics.mode(agesData))
+print(statistics.median(agesData))

2f242e654916da426e1e5bbdacaeb673c0495a07
Liberbell <liberbell@gmail.com>
2020-03-06 10:47:44 +0900

    operators1

---
+++ operators.py
@@ -0,0 +1,21 @@
+print(10 < 75)
+print(75 < 10)
+
+if 10 < 75:
+    print("The bigger number is big.")
+
+kitten = 10
+tiger = 75
+
+if kitten < tiger:
+    print("The kitten weight less than tiger.")
+
+mouse = 1
+if mouse < kitten and mouse < tiger:
+    print("The mouse weights the least ")
+
+print(False > True)
+
+print("Jenniffer" > "Jenny")
+
+print('a' <= 'b')

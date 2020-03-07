097ff83692027f943bc9370765667a5b28a3d715
Liberbell <liberbell@gmail.com>
2020-03-07 15:25:35 +0900

    iterators6

--- iterators.py
+++ iterators.py
@@ -1,6 +1,9 @@
 import itertools
 
-for x in itertools.count(50, 5):
-    print(x)
-    if x == 10000:
-        break
+# for x in itertools.count(50, 5):
+#     print(x)
+#     if x == 10000:
+#         break
+
+for c in itertools.cycle("RACECAR"):
+    print(c)
